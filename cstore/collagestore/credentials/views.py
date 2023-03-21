8aa0091752efb0a630a3864644eee8f5938474e3
Agnakamal <agnakamalk@gmail.com>
2023-03-21 11:48:43 +0300

    taskcollage added

---
+++ cstore/collagestore/credentials/views.py
@@ -0,0 +1,44 @@
+from django.contrib import auth, messages
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+
+
+# Create your views here.
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, "Invalid")
+            return redirect('credentials:login')
+
+    return render(request, 'login.html')
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        c_password = request.POST['c_password']
+
+        if password == c_password:
+            if User.objects.filter(username=username):
+                messages.info(request, "username allready exists")
+                return redirect('credentials:register')
+
+            else:
+                user = User.objects.create_user(username=username,password=password)
+                user.save()
+                print("user created")
+                return redirect('credentials:login')
+        else:
+            messages.info(request, 'Password dose not match')
+            return redirect('credentials:register')
+    return render(request, 'register.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('credentials:login')
